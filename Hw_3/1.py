c1a7682d31afc57fead3ebfa2555a24249e1bd0d
Dmitriy Shmatko <dmitriy.shmatko@gameloft.com>
2019-11-09 11:04:17 +0200

    + hw3

--- Hw_3/1.py
+++ Hw_3/1.py
@@ -3,20 +3,17 @@ filename = sys.argv[1]
 # далее открываем файл на чтение (опция 'r')
 f = open(filename, 'r')  # в файле теперь file descriptor
 for line in f:  # для каждой строки в файле
-    print(line)
+    print(line, end="")
     counters = line.split()
-    print(counters)
     counters = list(map(int, counters))
-
-
-for counter in counters:
-    for i in range(1, counter[2]+1):
-        if i % counter[0] == 0:
+    print(counters)
+    for i in range(1, counters[2]+1):
+        if i % counters[0] == 0:
             print("F", end="")
-        if i % counter[1] == 0:
+        if i % counters[1] == 0:
             print("B", end="")
-        if not i % counter[0] == 0 and i % counter[1]:
+        if not i % counters[0] == 0 and i % counters[1]:
             print(i, end="")
         print("", end=" ")
 
-print()
+    print()
