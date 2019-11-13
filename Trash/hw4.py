8edc329836b7ba08a304d840ce359ec624c5b15c
Dmitriy Shmatko <dmitriy.shmatko@gameloft.com>
2019-11-13 13:56:44 +0200

    + hw4

---
+++ Trash/hw4.py
@@ -0,0 +1,40 @@
+from random import sample
+import sys
+import random
+import numpy as np
+
+filename = sys.argv[1]
+
+file_with_numbers = open(filename, 'w')
+
+
+for _ in range(20):
+    list = sample(range(1, 99), 3)
+    fizz_buzz_numbers = ' '.join(map(str, list))
+    file_with_numbers.write(fizz_buzz_numbers)
+    file_with_numbers.write("\n")
+
+file_with_numbers.close()
+
+
+def fizz_buzz(fizz, buzz, number):
+    write_file = open('result.txt', 'w')
+    for line in file_with_numbers:
+        #        counters = line.split()
+        #        counters = list(map(int, counters))
+        for i in range(1, number+1):
+            if i % fizz == 0:
+                write_file.write("F")
+            if i % buzz == 0:
+                write_file.write("B")
+            if not i % fizz == 0 and i % buzz:
+                write_file.write(str(i))
+            write_file.write(" ")
+        write_file.write("\n")
+
+
+file_with_numbers = open(filename, 'r')
+for line in file_with_numbers:
+    li = line.split()
+    print(li)
+    fizz_buzz(int(li[0]), int(li[1]), int(li[2]))
