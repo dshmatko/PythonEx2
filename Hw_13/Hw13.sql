3b6ff93d9ec8733dcb662655c4cc7af220158923
Dmitriy Shmatko <dmitriy.shmatko@gameloft.com>
2019-12-18 14:41:05 +0200

    + hw 13

--- Hw_13/Hw13.sql
+++ Hw_13/Hw13.sql
@@ -159,3 +159,81 @@ journal_db=# SELECT class_name FROM classes LEFT JOIN teachers ON  (classes.clas
 (3 rows)
 
 
+journal_db=# SELECT class_name FROM classes LEFT JOIN teachers ON  (classes.classroom_teacher = teachers.id) WHERE classes.classroom_teacher  is NULL OFFSET 1;
+ class_name 
+------------
+ 5
+ 4
+(2 rows)
+
+journal_db=# SELECT class_name FROM classes RIGHT JOIN teachers ON  (classes.classroom_teacher = teachers.id) ORDER BY  teachers.id DESC LIMIT 1  ;
+ class_name 
+------------
+ 5
+(1 row)
+
+
+
+journal_db=# SELECT students.id ,students.first_name, students.last_name, classes.class_name, classes.classroom_teacher, teachers.first_name, teachers.last_name  FROM students INNER JOIN classes ON students.class_id = classes.id INNER JOIN teachers ON classes.classroom_teacher = teachers.id ;
+ id | first_name | last_name | class_name | classroom_teacher | first_name | last_name 
+----+------------+-----------+------------+-------------------+------------+-----------
+  1 | Anatoliy   | Laktionov | 3          |                 1 | Ivan       | Petrov
+  2 | Ivan       | Novikov   | 3          |                 1 | Ivan       | Petrov
+  5 | Ivan       | Novikov   | 3          |                 1 | Ivan       | Petrov
+  7 | Anatoliy   | Laktionov | 3          |                 1 | Ivan       | Petrov
+  8 | Ivan       | Novikov   | 3          |                 1 | Ivan       | Petrov
+  4 | Anatoliy   | Laktionov | 3          |                 1 | Ivan       | Petrov
+  3 | Nikolay    | Petrov    | 3          |                 3 | Alexandr   | Sidorov
+  6 | Nikolay    | Petrov    | 3          |                 3 | Alexandr   | Sidorov
+  9 | Nikolay    | Petrov    | 3          |                 3 | Alexandr   | Sidorov
+
+
+
+
+
+
+journal_db=# SELECT students.id ,students.first_name, students.last_name,  teachers.first_name, teachers.last_name  FROM students INNER JOIN classes ON students.class_id = classes.id INNER JOIN teachers ON classes.classroom_teacher = teachers.id ORDER BY students.id;
+ id | first_name | last_name | first_name | last_name 
+----+------------+-----------+------------+-----------
+  1 | Anatoliy   | Laktionov | Ivan       | Petrov
+  2 | Ivan       | Novikov   | Ivan       | Petrov
+  3 | Nikolay    | Petrov    | Alexandr   | Sidorov
+  4 | Anatoliy   | Laktionov | Ivan       | Petrov
+  5 | Ivan       | Novikov   | Ivan       | Petrov
+  6 | Nikolay    | Petrov    | Alexandr   | Sidorov
+  7 | Anatoliy   | Laktionov | Ivan       | Petrov
+  8 | Ivan       | Novikov   | Ivan       | Petrov
+  9 | Nikolay    | Petrov    | Alexandr   | Sidorov
+(9 rows)
+
+
+
+journal_db=# SELECT DISTINCT  teachers.last_name as prepod, students.id ,students.first_name, students.last_name  FROM students INNER JOIN classes ON students.class_id = classes.id LEFT JOIN teachers ON classes.classroom_teacher = teachers.id ORDER BY students.id ;
+ prepod  | id | first_name | last_name 
+---------+----+------------+-----------
+ Petrov  |  1 | Anatoliy   | Laktionov
+ Petrov  |  2 | Ivan       | Novikov
+ Sidorov |  3 | Nikolay    | Petrov
+ Petrov  |  4 | Anatoliy   | Laktionov
+ Petrov  |  5 | Ivan       | Novikov
+ Sidorov |  6 | Nikolay    | Petrov
+ Petrov  |  7 | Anatoliy   | Laktionov
+ Petrov  |  8 | Ivan       | Novikov
+ Sidorov |  9 | Nikolay    | Petrov
+(9 rows)
+
+
+journal_db=# SELECT DISTINCT  UPPER (teachers.last_name) as prepod, students.id ,students.first_name, students.last_name  FROM students INNER JOIN classes ON students.class_id = classes.id LEFT JOIN teachers ON classes.classroom_teacher = teachers.id ORDER BY students.id ;
+ prepod  | id | first_name | last_name 
+---------+----+------------+-----------
+ PETROV  |  1 | Anatoliy   | Laktionov
+ PETROV  |  2 | Ivan       | Novikov
+ SIDOROV |  3 | Nikolay    | Petrov
+ PETROV  |  4 | Anatoliy   | Laktionov
+ PETROV  |  5 | Ivan       | Novikov
+ SIDOROV |  6 | Nikolay    | Petrov
+ PETROV  |  7 | Anatoliy   | Laktionov
+ PETROV  |  8 | Ivan       | Novikov
+ SIDOROV |  9 | Nikolay    | Petrov
+(9 rows)
+
